42033995a7d03d24e954b8f920fe5c73e8f7d8ce
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-03-08 13:45:08 +0100

    Cleanup main and create dist folder

--- nplan.go
+++ nplan.go
@@ -11,6 +11,9 @@ import (
 	"github.com/richartkeil/nplan/parser"
 )
 
+var defaultModelPath = "./dist/model.json"
+var defaultDrawioPath = "./dist/drawio.json"
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -23,38 +26,53 @@ func main() {
 	scan6InputFlag := flag.String("scan6", "", "Set the path to the scan6 input .txt file. For this to take effect the current model should already include MAC addresses.")
 	exportFlag := flag.Bool("export", false, "Export the current model to a .drawio file.")
 	// Config flags
-	jsonFileFlag := flag.String("json", "./dist/model.json", "Set the path where to store the .json model file.")
-	drawioOutputFlag := flag.String("drawio", "./dist/plan.drawio", "Set the path were to store the exported .drawio file.")
+	jsonFileFlag := flag.String("json", defaultModelPath, "Set the path where to store the .json model file.")
+	drawioOutputFlag := flag.String("drawio", defaultDrawioPath, "Set the path were to store the exported .drawio file.")
 	resetModelFlag := flag.Bool("fresh", false, "Delete the previous .json model and build a new one. Use with caution.")
 
 	flag.Parse()
 
-	jsonData, err := os.ReadFile(*jsonFileFlag)
-	if errors.Is(err, os.ErrNotExist) || *resetModelFlag {
-		jsonData = []byte("{}")
-		err = os.WriteFile(*jsonFileFlag, jsonData, 0644)
-		check(err)
-	}
-
-	var scan core.Scan
-	err = json.Unmarshal(jsonData, &scan)
-	check(err)
+	createDistFolder(jsonFileFlag, drawioOutputFlag)
+	scan := loadModel(jsonFileFlag, resetModelFlag)
 
 	if *nmapInputFlag != "" {
 		nmapScan := parser.ParseNmap(*nmapInputFlag)
-		core.ComplementWithNmap(&scan, &nmapScan)
+		core.ComplementWithNmap(scan, &nmapScan)
 	}
 	if *scan6InputFlag != "" {
 		scan6Hosts := parser.ParseScan6(*scan6InputFlag)
-		core.ComplementWithIPv6(&scan, &scan6Hosts)
+		core.ComplementWithIPv6(scan, &scan6Hosts)
 	}
 
-	json, err := json.MarshalIndent(scan, "", "  ")
-	check(err)
-
-	os.WriteFile(*jsonFileFlag, json, 0644)
+	saveModel(jsonFileFlag, scan)
 
 	if *exportFlag {
-		exporter.Export(*drawioOutputFlag, &scan)
+		exporter.Export(*drawioOutputFlag, scan)
 	}
+}
+
+func createDistFolder(modelPath *string, drawioPath *string) {
+	if *modelPath == defaultModelPath && *drawioPath == defaultDrawioPath {
+		err := os.MkdirAll("./dist", 0755)
+		check(err)
+	}
+}
+
+func loadModel(modelPath *string, resetModel *bool) *core.Scan {
+	jsonData, err := os.ReadFile(*modelPath)
+	if errors.Is(err, os.ErrNotExist) || *resetModel {
+		jsonData = []byte("{}")
+		err = os.WriteFile(*modelPath, jsonData, 0644)
+		check(err)
+	}
+	var scan core.Scan
+	err = json.Unmarshal(jsonData, &scan)
+	check(err)
+	return &scan
+}
+
+func saveModel(modelPath *string, scan *core.Scan) {
+	json, err := json.MarshalIndent(scan, "", "  ")
+	check(err)
+	os.WriteFile(*modelPath, json, 0644)
 }
